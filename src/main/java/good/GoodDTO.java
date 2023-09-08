b10cbe096ffe91043a19b6dc2d4f5c5222edda7f
sweetie0916 <dmstjs2720@naver.com>
2023-09-08 12:57:52 +0900

    2023.09.08

---
+++ src/main/java/good/GoodDTO.java
@@ -0,0 +1,22 @@
+package good;
+
+public class GoodDTO {
+	//멤버변수
+	private String idx;
+	private int gcnt;
+	//getter / setter
+	public String getIdx() {
+		return idx;
+	}
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+	public int getGcnt() {
+		return gcnt;
+	}
+	public void setGcnt(int gcnt) {
+		this.gcnt = gcnt;
+	}
+	
+	
+}
